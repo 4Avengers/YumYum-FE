9f17eee60fe65d847de6633e7d329bfbc8d1a90a
hansejun <tpwnszja2@gmail.com>
2023-03-08 01:13:11 +0900

    [style] footer 아이콘 변경

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,28 +1,56 @@
 import tw from "tailwind-styled-components";
-import { BiHomeAlt2, BiReceipt, BiMapAlt, BiUser } from "react-icons/bi";
+import { BiHomeAlt2, BiReceipt, BiUser } from "react-icons/bi";
 import { CiSquarePlus } from "react-icons/ci";
-import { useNavigate } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillPlusSquareFill } from "react-icons/bs";
+import { useMatch, useNavigate } from "react-router-dom";
+import { FaMap, FaRegMap, FaUser } from "react-icons/fa";
 
 const Footer = () => {
   const navigate = useNavigate();
+  const homeMatch = useMatch("/");
+  const newsfeedMatch = useMatch("/newsfeed/*");
+  const writeMatch = useMatch("/post/write");
+  const mapMatch = useMatch("/quest/*");
+  const profileMatch = useMatch("/profile/*");
   return (
     <footer>
       <nav className="screen-width  fixed bottom-0 w-screen  border-t">
         <ul className="grid h-[6rem]  grid-cols-5">
           <Icon onClick={() => navigate("/")}>
-            <BiHomeAlt2 size="2.5rem" color="#444444" strokeWidth="0.1" />
+            {homeMatch ? (
+              <AiFillHome size="2.5rem" color="#444444" strokeWidth="0.1" />
+            ) : (
+              <BiHomeAlt2 size="2.5rem" color="#616161" strokeWidth="0.1" />
+            )}
           </Icon>
           <Icon onClick={() => navigate("/newsfeed")}>
-            <BiReceipt size="2.5rem" color="#444444" strokeWidth="0.1" />
+            <BiReceipt
+              size="2.5rem"
+              color={newsfeedMatch ? "#444444" : "#616161"}
+              strokeWidth={newsfeedMatch ? "1" : "0.1"}
+            />
           </Icon>
           <Icon onClick={() => navigate("/post/write")}>
-            <CiSquarePlus size="2.6rem" color="#444444" strokeWidth="0.8" />
+            {writeMatch ? (
+              <BsFillPlusSquareFill size="2.2rem" color="#444444" />
+            ) : (
+              <CiSquarePlus size="2.6rem" color="#616161" strokeWidth="0.8" />
+            )}
           </Icon>
           <Icon onClick={() => navigate("/quest")}>
-            <BiMapAlt size="2.5rem" color="#444444" strokeWidth="0.1" />
+            {mapMatch ? (
+              <FaMap size="2.5rem" color="#444444" />
+            ) : (
+              <FaRegMap size="2.5rem" color="#616161" strokeWidth="0.1" />
+            )}
           </Icon>
           <Icon onClick={() => navigate("/profile/1")}>
-            <BiUser size="2.5rem" color="#444444" strokeWidth="0.1" />
+            {profileMatch ? (
+              <FaUser size="2.2rem" color="#444444" />
+            ) : (
+              <BiUser size="2.5rem" color="#616161" strokeWidth="0.1" />
+            )}
           </Icon>
         </ul>
       </nav>
