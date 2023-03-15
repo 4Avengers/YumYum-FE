d6072925c444b1c39c18e48203e0c490b71acea8
hansejun <tpwnszja2@gmail.com>
2023-03-15 12:52:00 +0900

    [fix] : 해시태그 이름이 안나오는 버그 수정

--- src/components/common/post/postCard/Paragragh.jsx
+++ src/components/common/post/postCard/Paragragh.jsx
@@ -9,33 +9,38 @@ const Paragragh = ({ post }) => {
   };
 
   return (
-    <div className="flex space-x-[1rem] px-[2rem] pt-[0.8rem]">
-      <p
-        className={cls(
-          "Cap4 text-primary-600",
+    <div className=" flex flex-col space-y-[0.4rem] px-[2rem] ">
+      <div className="flex items-center space-x-[1rem] pt-[0.8rem]">
+        <p
+          className={cls(
+            "Cap4 text-primary-600",
 
-          !showFullText &&
-            "max-w-[60%] overflow-hidden text-ellipsis whitespace-nowrap"
-        )}
-      >
-        <span className="Cap3 mr-[0.7rem] whitespace-nowrap text-primary-700">
-          {post?.user?.nickname}
-        </span>
-        {post?.content}
-      </p>
-
-      {!showFullText ? (
-        <button
-          className="Cap4 cursor-pointer text-primary-500"
-          onClick={toggleShowFullText}
+            !showFullText &&
+              "max-w-[60%] overflow-hidden text-ellipsis whitespace-nowrap"
+          )}
         >
-          더보기
-        </button>
-      ) : (
-        <ul className="flex  space-x-[0.4rem]">
+          <span className="Cap3 mr-[0.7rem] whitespace-nowrap text-primary-700">
+            {post?.user?.nickname}
+          </span>
+          {post?.content}
+        </p>
+
+        {!showFullText && (
+          <button
+            className="Cap4 cursor-pointer text-primary-500"
+            onClick={toggleShowFullText}
+          >
+            더보기
+          </button>
+        )}
+      </div>
+      {showFullText && (
+        <ul className="Cap6 p  flex flex-wrap  items-center">
           {React.Children.toArray(
             post?.hashtags?.map((hashtag) => (
-              <li className="text-config-blue">#{hashtag?.name}</li>
+              <li className="mr-[0.4rem] whitespace-nowrap text-config-blue">
+                #{hashtag}
+              </li>
             ))
           )}
         </ul>
