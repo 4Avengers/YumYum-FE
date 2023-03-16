d9990c6fbdc48b0528d046925e02c4c868d58a12
hansejun <tpwnszja2@gmail.com>
2023-03-17 06:38:25 +0900

    [fix] : 사용 안하는 변수 제거

--- src/pages/collection/Index.jsx
+++ src/pages/collection/Index.jsx
@@ -1,10 +1,10 @@
 //import CollectionService from "apis/service/CollectionService";
 import Header from "components/layout/Header";
 import Layout from "components/layout/Layout";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 
 const Collection = () => {
-  const { profileId } = useParams();
+  //const { profileId } = useParams();
   //const { data: collections } = CollectionService.ReadCollectionList(profileId);
 
   return (
