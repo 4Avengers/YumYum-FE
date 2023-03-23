c2325268cf11b62b40073babd49c0076cd8fc3f6
favorsyoon <favorsyoon@gmail.com>
2023-03-24 01:08:55 +0900

    [feat] Oauth Google 기능 개발 완료

--- src/components/start/SignIn.jsx
+++ src/components/start/SignIn.jsx
@@ -33,7 +33,9 @@ const SignIn = () => {
   };
   
   const LoginWithGoogle = () => {
-    const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&response_type=token&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email`;
+    const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}`;
+   
+    // const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&response_type=token&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email`;
     window.location.href = GOOGLE_AUTH_URL;
   };
   
