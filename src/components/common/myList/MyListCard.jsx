2b60c47648c1f65e677a56d718f9b15816ff3767
hansejun <tpwnszja2@gmail.com>
2023-03-24 09:48:31 +0900

    [feat] : 리스트 alert

--- src/components/common/myList/MyListCard.jsx
+++ src/components/common/myList/MyListCard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { toast } from "react-toastify";
 import cls from "utils/cls";
 
 const MyListCard = ({ myListData, isInclude, setMyList }) => {
+  const handleAddList = (e) => {
+    e.stopPropagation();
+    setMyList((prev) => [...prev, myListData.id]);
+    toast.success(`${myListData?.name}에 추가하였습니다.`);
+  };
+  const handleDeleteList = (e) => {
+    e.stopPropagation();
+    setMyList((prev) => [...prev.filter((item) => item !== myListData?.id)]);
+    toast.success(`${myListData?.name}에서 삭제하였습니다.`);
+  };
+
   return (
     <li className="flex items-center justify-between border-b px-[2rem] py-[1rem]">
       <span className="Cap2 flex-1">{myListData.name}</span>
@@ -10,22 +22,14 @@ const MyListCard = ({ myListData, isInclude, setMyList }) => {
           className={cls(
             "Cap4 rounded-[1rem] border  bg-primary-700 p-[0.7rem_1rem] text-primary-100"
           )}
-          onClick={(e) => {
-            e.stopPropagation();
-            setMyList((prev) => [
-              ...prev.filter((item) => item !== myListData?.id),
-            ]);
-          }}
+          onClick={handleDeleteList}
         >
           선택
         </button>
       ) : (
         <button
           className="Cap4 rounded-[1rem] border border-primary-400 p-[0.7rem_1rem] text-primary-400 transition-colors hover:border-primary-600  hover:text-primary-600"
-          onClick={(e) => {
-            e.stopPropagation();
-            setMyList((prev) => [...prev, myListData.id]);
-          }}
+          onClick={handleAddList}
         >
           선택
         </button>
