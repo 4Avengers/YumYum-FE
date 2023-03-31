7357d0293a1b6a7528e95353aaa85071ca0697d2
hansejun <tpwnszja2@gmail.com>
2023-04-01 02:30:33 +0900

    [feat] : 채팅 ui 구현

--- src/components/chats/ChatItem.jsx
+++ src/components/chats/ChatItem.jsx
@@ -1,7 +1,34 @@
+import dayjs from "dayjs";
 import React from "react";
+import cls from "utils/cls";
+import { handleImgError } from "utils/handleImgError";
 
-const ChatItem = () => {
-  return <div>ChatItem</div>;
+const ChatItem = ({ chat, isMe, receiver }) => {
+  return (
+    <li className={cls("flex space-x-[1rem]")} isMe={isMe}>
+      <img
+        className={"h-[3.5rem] w-[3.5rem] rounded-full object-cover"}
+        src={receiver?.profile_image}
+        alt="avatar"
+        onError={handleImgError}
+      />
+      <div
+        className={cls("Cap3  flex flex-1 flex-col space-y-[0.4rem]")}
+        isMe={isMe}
+      >
+        <span>{chat?.nickname}</span>
+        <div className="">
+          <p className="Cap4 break-all rounded-md bg-white p-[0.6rem]">
+            "dasndkjasbdkjasbdhjbashjdbashjdbhjasbdjhasbdjhbashjdbshjbdhjasbdjhbashjdbahjsd
+            hjas dhjas dhj asjhd ahjsd jhs hj"
+          </p>
+        </div>
+      </div>
+      <span className="self-end">
+        {dayjs(new Date(chat.createdAt?.seconds)).format("A HH:mm")}
+      </span>
+    </li>
+  );
 };
 
 export default ChatItem;
--- src/components/chats/ChatList.jsx
+++ src/components/chats/ChatList.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+import React, { useCallback, useEffect, useState } from "react";
 import ChatItem from "./ChatItem";
 
-const ChatList = () => {
-  const chats = [1, 2, 3, 4];
+const ChatList = ({ chatId, user, receiver }) => {
+  const [chats, setChats] = useState([]);
+
+  const getMessages = useCallback(() => {
+    const chatCollection = collection(db, `chatRoom`);
+    console.log(chatCollection, chatId);
+    const roomMessagesQuery = query(
+      chatCollection,
+      where("chatId", "==", chatId),
+      orderBy("createdAt", "desc"),
+      limit(20)
+    );
+    return roomMessagesQuery;
+  }, [chatId]);
+
+  useEffect(() => {
+    const roomMessagesQuery = getMessages();
+    console.log(roomMessagesQuery);
+    onSnapshot(roomMessagesQuery, (snapshot) => {
+      console.log(snapshot);
+      const updatedMessages = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      setChats(updatedMessages.reverse());
+    });
+  }, [getMessages]);
+
+  console.log(chats);
+
   return (
-    <ul className="mb-[12rem] h-[calc(100vh-18.7rem)]  overflow-y-auto px-[2rem] scrollbar-hide">
-      {chats?.map((item) => (
-        <ChatItem key={item} />
+    <ul className="mb-[12rem] h-[calc(100vh-18.7rem)]  space-y-[1rem] overflow-y-auto px-[2rem] pt-[2rem] scrollbar-hide">
+      {chats?.map((chat) => (
+        <ChatItem
+          key={chat?.id}
+          chat={chat}
+          isMe={user?.id === chat?.userId}
+          receiver={receiver}
+        />
       ))}
     </ul>
   );
